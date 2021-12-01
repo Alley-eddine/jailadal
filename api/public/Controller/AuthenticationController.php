aaa803fe089161d6e16194d7398a487ad01ce608
Ivan <ivan@vidata.io>
2021-12-01 10:52:57 +0100

    [architecture-server] more examples for dev use and AuthenticationController implemented first as template

--- api/public/Controller/AuthenticationController.php
+++ api/public/Controller/AuthenticationController.php
@@ -1,6 +1,23 @@
 <?php
 namespace Controller;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 
 class AuthenticationController {
+    public static function authenticate(Request $request, Response $response): Response {
+        // TODO: implement logic
 
+        // Test purpose logic
+        $data = $request->getParsedBody();
+
+        $login = $data['login'];
+        $password = $data['password'];
+
+        $response->getBody()->write(
+            "<span>Login:<br>$login</span><br><br>"
+            ."<span>Password:<br>$password</span><br><br>"
+        );
+
+        return $response;
+    }
 }
--- api/public/index.php
+++ api/public/index.php
@@ -2,15 +2,14 @@
 use Slim\Factory\AppFactory;
 use Slim\Factory\ServerRequestCreatorFactory;
 use Slim\Handlers\Strategies\RequestResponseArgs;
-use Slim\Routing\RouteCollectorProxy as Proxy;
+use Slim\Routing\RouteCollectorProxy as Group;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-
 use Controller\AuthenticationController;
 
 require '../vendor/autoload.php';
 
-// Create and setup API
+// Créer et setup l'API
 AppFactory::setSlimHttpDecoratorsAutomaticDetection(false);
 ServerRequestCreatorFactory::setSlimHttpDecoratorsAutomaticDetection(false);
 
@@ -25,16 +24,16 @@ $app->addRoutingMiddleware();
 $errorMiddleware = $app->addErrorMiddleware(true, false, false);
 $errorMiddleware->setDefaultErrorHandler($errorHandler);
 
-//------------- Router part ---------------
-// GET
-$app->get('/', function (Request $request, Response $response) {
-    // Get HTTP method (GET, POST, PUT, DELETE, ...)
+//------------- Le router ---------------
+// Exemple GET
+$app->get('/test', function (Request $request, Response $response): Response {
+    // HTTP method (GET, POST, PUT, DELETE, ...)
     $method = $request->getMethod();
 
-    // Get URI
+    // URI
     $uri = $request->getUri();
 
-    // Get all headers
+    // Tous les headers
     $headers = $request->getHeaders();
     foreach ($headers as $name => $values) {
         echo $name . ": " . implode(", ", $values);
@@ -42,57 +41,108 @@ $app->get('/', function (Request $request, Response $response) {
     }
     echo '<br>';
 
-    // Check a header exists and get its value if exists
-    $userAgent = 'User-Agent';
-    if ($request->hasHeader($userAgent)) {
-        // Specific header values
-        $specificHeaderValues = $request->getHeader($userAgent);
+    // Vérifier qu'un header existe puis y accéder
+    $host = 'Host';
+    if ($request->hasHeader($host)) {
+        // Accéder aux valeurs d'un header spécifique
+        $specificHeaderValues = $request->getHeader($host);
         foreach ($specificHeaderValues as $value) {
             echo $value;
             echo '<br>';
         }
         echo '<br>';
     
-        // Specific header line
-        $specificHeaderLine = $request->getHeaderLine($userAgent);
+        // Accéder aux valeurs d'un header sous forme d'une string
+        $specificHeaderLine = $request->getHeaderLine($host);
         echo "<span>$specificHeaderLine</span><br><br>";
     }
 
-    // Get body
-    $response->getBody()->write("<span>hello there</span><br>");
+    // Récupérer le body
+    $body = $request->getBody();
 
+    $response->getBody()->write("<span>This is a GET method response test.</span><br><br>");
+    
     return $response;
 });
 
 // POST
-$app->post('/', function (Request $request, Response $response, $args): Response {
+$app->post('/test', function (Request $request, Response $response): Response {
     $data = $request->getParsedBody();
     
     $html = var_export($data, true);
-    $response->getBody()->write($html);
+    $response->getBody()->write($html.'<br><br>');
     
     return $response;
 });
 
-// Test group
-$app->group('/hello', function (Proxy $group) {
-    $group->get('', function (Request $request, Response $response) {
-        $response->getBody()->write("<span>hello there</span>");
+// Groups
+$app->group('/hello', function (Group $helloGroup): void {
+    $helloGroup->get('', function (Request $request, Response $response): Response {
+        $response->getBody()->write("<span>hello!</span>");
         return $response;
     });
 
-    $group->get('/{name}', function (Request $request, Response $response, $name) {
-        $response->getBody()->write("<span>hello $name</span>");
+    $helloGroup->get('/goodbye', function (Request $request, Response $response): Response {
+        $response->getBody()->write("<span>hello!</span><br>");
+        $response->getBody()->write("<span>goodbye.</span>");
         return $response;
     });
 
-    $group->group('/there', function (Proxy $group) {
-        $group->get('/general-kenobi', function (Request $request, Response $response) {
+    $helloGroup->group('/there', function (Group $thereGroup): void {
+        // ATTENTION : Si besoin d'une méthode avec un paramètre, elle doit être appelée après
+        //             les méthodes statiques du même groupe pour éviter de masquer la lecture
+        //             de ces dernières.
+
+        // Méthode statique 1
+        $thereGroup->get('', function (Request $request, Response $response): Response {
+            $response->getBody()->write("<span>hello there</span>");
+            return $response;
+        });
+
+        // Méthode statique 2
+        $thereGroup->get('/general-kenobi', function (Request $request, Response $response): Response {
             $response->getBody()->write("<span>hello there</span><br><span>general kenobi!</span>");
+            return $response;
+        });
+
+        // Méthode avec un paramètre
+        $thereGroup->get('/{name}', function (Request $request, Response $response, $name) {
+            $response->getBody()->write("<span>hello there</span><br><span>$name!</span>");
             return $response;
         });
     });
 });
 
+// Authentication
+$app->post('/authentication', [AuthenticationController::class, 'authenticate']);
+
 // Run API
 $app->run();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <!-- POST test -->
+    <form action="/test" method="POST">
+        <input type="text" name="test1" placeholder="test1">
+        <input type="text" name="test2" placeholder="test2">
+        <input type="text" name="test3" placeholder="test3">
+        <button type="submit">Tester le POST</button>
+    </form>
+
+    <!-- Authentication -->
+    <form action="/authentication" method="POST">
+        <input type="text" name="login" placeholder="login">
+        <input type="text" name="password" placeholder="password">
+        <button type="submit">Authenticate</button>
+    </form>
+</body>
+</html>
