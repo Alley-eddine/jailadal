e7958278cef44f30b623a81f98b5f8f6005cf9db
Ivan <ivan@vidata.io>
2021-12-02 11:47:21 +0100

    [server-services] adding TODO for ItemService template

--- back/api/public/Entities/Services/ItemService.php
+++ back/api/public/Entities/Services/ItemService.php
@@ -9,6 +9,12 @@ class ItemService
 {
     public function getAllItems(Request $request, Response $response): Response
     {
+        // TODO:
+        // Instancier un ItemModel.
+        // Appeler la méthode appropriée à la construction de ItemModel dans sa liste de méthodes.
+        // Formater l'ItemModel en JSON.
+        // Ajouter le JSON au body de $response.
+
         $response->getBody()->write('<span>GET All Items</span>');
         return $response;
     }
